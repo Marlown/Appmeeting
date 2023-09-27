97200c53cdeb79c3d21d208b9471e9b5e6fef139
Lokine94 <65287876+Lokine94@users.noreply.github.com>
2023-09-27 09:31:58 +0200

    Docker compose

---
+++ appmeeting/src/Back-end/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = express()
+
+const cors = require('cors');
+server.use(cors());
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinodepmn');
+
+server.use(express.urlencoded());
+server.use(express.json());
+
+// Routes
+const postRoute = require('../api/routes/postRoute.js');
+server.use('/posts', postRoute);
+
+const commentRoute = require('../api/routes/commentRoute');
+server.use('/', commentRoute);
+
+server.listen(port, hostname);
