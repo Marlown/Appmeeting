62b8ea0c2e286ee53d35592c9cce11f04ac6255a
Marlown <114161785+Marlown@users.noreply.github.com>
2023-09-30 16:12:34 +0200

    style css modifier

--- appmeeting-front/src/App.js
+++ appmeeting-front/src/App.js
@@ -10,11 +10,29 @@ import AfficheData from './Appcomponent/Affichage_data/AfficheData';
 function App() {
   return (
     <div className="App">
-      <CSVImportForm />
-      <ImportBouton />
-      <AfficheData />
-      <ExportBouton />
+      <h1>Iterface d'importation</h1>
+
+      <div className="data-container">
+        {/* Cadre "Données importer/exporter" */}
+        <div className="data-section">
+          <h2>Données importer/exporter</h2>
+
+          {/* Bouton "Importer" */}
+          <CSVImportForm />
+          <button className="import-button">Importer des données</button>
+
+          {/* Bouton "Exporter" */}
+          <ExportBouton />
+          <button className="export-button">Exporter des données</button>
+        </div>
+      </div>
+
+      {/* Autres éléments de l'application */}
+      <div className="other-section">
+        {/* ... */}
+      </div>
     </div>
   );
 }
+
 export default App;
